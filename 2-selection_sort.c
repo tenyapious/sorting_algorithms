2c38b2000d7719c5d95ee4c652838848cd1a736a
tenyapious <tenyapious@gmail.com>
2023-12-18 23:44:37 +0000

    Write the selection sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sort array of integers in ascending order
+ * using selection sort algorithm
+ * @array: the array of integers
+ * @size: the length of the array
+*/
+void selection_sort(int *array, size_t size)
+{
+	int temp, sw = 0;
+	size_t i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = i; j < size - 1; j++)
+		{
+			if (array[i] > array[j + 1])
+			{
+				temp = array[j + 1];
+				array[j + 1] = array[i];
+				array[i] = temp;
+			}
+		}
+		print_array(array, size);
+	}
+}
