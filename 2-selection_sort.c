824a73b2834c959b576c11f14ffdbfe1131ecade
tenyapious <tenyapious@gmail.com>
2023-12-19 00:00:17 +0000

    reduce outer loop by 1

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,7 +14,7 @@ void selection_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		for (j = i; j < size - 1; j++)
 		{
