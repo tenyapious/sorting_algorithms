6a22793e529a23aa697ccc06b60aa3b69bdf2bd1
tenyapious <tenyapious@gmail.com>
2023-12-19 19:03:28 +0000

    Print the array every time integers swap

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -18,12 +18,13 @@ void swap_int(int *array, int idx1, int idx2)
  * partition - rerange integers less than a chosen integer
  * of an array in ascending order
  * @array: the array
+ * @size: length of array
  * @start: first index of array
  * @end: last index of array
  *
  * Return: index of the chosen integer
 */
-int partition(int *array, int start, int end)
+int partition(int *array, size_t size, int start, int end)
 {
 	int i, j;
 
@@ -33,31 +34,38 @@ int partition(int *array, int start, int end)
 		{
 			i++;
 			if (i != j)
+			{
 				swap_int(array, i, j);
+				print_array(array, size);
+			}
 		}
 	}
 	i++;
 	if (array[j] < array[i])
+	{
 		swap_int(array, i, j);
+		print_array(array, size);
+	}
 
 	return (i);
 }
 
 /**
  * sort - sort array of integers in ascending order recursively
+ * @size: length of array
  * @array: the array
  * @start: first index of array
  * @end: last index of array;
 */
-void sort(int *array, int start, int end)
+void sort(int *array, size_t size, int start, int end)
 {
 	int pivot_location;	
 
 	if (start < end)
 	{
-		pivot_location = partition(array, start, end);
-		sort(array, start, pivot_location - 1);
-		sort(array, pivot_location + 1, end);
+		pivot_location = partition(array, size, start, end);
+		sort(array, size, start, pivot_location - 1);
+		sort(array, size, pivot_location + 1, end);
 	}
 }
 
@@ -71,7 +79,7 @@ void quick_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	sort(array, 0, (int)size - 1);
+	sort(array, size, 0, (int)size - 1);
 
 	print_array(array, size);
 }
