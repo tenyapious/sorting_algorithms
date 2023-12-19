615dd32e2d8170f64bda668f872c72c2c66bc1f9
tenyapious <tenyapious@gmail.com>
2023-12-19 10:27:13 +0000

    return if array is null or length < 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,6 +11,9 @@ void selection_sort(int *array, size_t size)
 	int min;
 	size_t i, j, minPos;
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		min = array[i];
